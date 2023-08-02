d86fd975f5b1825048032fa57d504f49fa95fbc8
Rush Burriel <burrielrush@gmail.com>
2023-08-01 20:53:45 -0600

    got project running at app.js, updated app.js to show profile

--- golden-buddies/src/App.js
+++ golden-buddies/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"><Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="./pages/Profile" element = {<Profile />} />
+      </Routes>
+      <Footer />
+    </Router>
     </div>
   );
 }
