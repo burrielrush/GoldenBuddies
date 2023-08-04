63569eea3374749a6e975ba8a117551e61a1b55c
JayceB17 <jayceb.ham17@yahoo.com>
2023-08-03 21:25:37 -0600

    added sign up and login and logout code in hooks

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator')
+
 const Schema = mongoose.Schema
 
 
@@ -13,5 +16,51 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+if (!email ||!password) {
+    throw Error('All fields must be filled')
+}
+if (!validator.isEmail(email)) {
+    throw Error('Email is not valled')
+}
+if (!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough')
+}
+
+const exists = await this.findOne({ email })
+if (exists) {
+    throw Error('Email already in use')
+}
+
+const salt = await bcrypt.genSalt(10)
+const hash = await bcrypt.hash(password, salt)
+
+const user = await this.create({ email , password: hash});
+
+return user
+}
+
+//static login method
+userSchema.statics.login = async function(email, password) {
+    if (!email ||!password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+    throw Error('Incorrect email')
+}
+
+const match = await bcrypt.compare(password, user.password)
+if(!match) {
+    throw Error('Incorrect password')
+}
+
+return user
+}
+
 
 module.exports = mongoose.model('User', userSchema)
--- golden-buddies/src/components/Navbar.js
+++ golden-buddies/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+    const { logout } = useLogout
+
+    const handleClick = () => {
+    logout()
+    }
 
     return (
         <header>
@@ -9,6 +15,9 @@ const Navbar = () => {
                     <h1>Golder Buddies</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>log out</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
