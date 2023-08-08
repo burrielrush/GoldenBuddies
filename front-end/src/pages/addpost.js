c5636652b89b8fd71ea2fc8b8642e50bc0fbe809
anieharmon <anieharmon@icloud.com>
2023-08-07 19:55:33 -0600

    front end

---
+++ front-end/src/pages/addpost.js
@@ -0,0 +1,6 @@
+//form by itself
+
+//upload jpeg with a form //css and html
+
+// img to url converter
+ // images of dogs should be put through an img to url converter online and then users can paste in the url and the DB stores them as a string
