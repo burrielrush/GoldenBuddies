761458159a692c1adda7a2085b245fb70a0fae1a
Rush Burriel <burrielrush@gmail.com>
2023-08-07 19:22:50 -0600

    updated queries

--- backend/utils/queries.js
+++ backend/utils/queries.js
@@ -2,18 +2,22 @@ import { gql } from '@apollo/client';
 
 // Query operation for fetching user data for the currently logged-in user
 export const GET_ME = gql`
-{
+  query getMe {
     me {
+      _id
+      username
+      email
+      postCount 
+      posts {   
+        _id
+        content
+        likes
+        createdAt
+      }
+      friends { // Assuming you have a field for fetching the user's friends
         _id
         username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            title
-            authors
-            image
-        }
+      }
     }
-}
+  }
 `;
