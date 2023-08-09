90ce851087223c690ddedf9ca1a83fa9ea674bee
JayceB17 <jayceb.ham17@yahoo.com>
2023-08-08 19:20:56 -0600

    small changes in src files

--- backend/routes/api/user-routes.js
+++ backend/routes/api/user-routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { signupUser, loginUser } = require('../../controllers/user');
+const requireAuth = require('../../middleware/requireAuth');
 
 const router = express.Router();
+
+router.use(requireAuth)
 
 router.post('/login', loginUser);
 router.post('/signup', signupUser);
