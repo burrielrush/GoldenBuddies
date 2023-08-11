b1c8ab032e5d22cdf874d442e96168a20599469d
JayceB17 <jayceb.ham17@yahoo.com>
2023-08-10 19:50:24 -0600

    fixed sign in and login error

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -42,8 +43,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.use(cors())
+
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
